0a308ee97b908d2448bd2dc118d1a828f6ec37aa
Geoff Bow <geoffrey.bow@gmail.com>
2021-10-11 15:27:08 +0100

    add routes

---
+++ src/lib/Header.svelte
@@ -0,0 +1,8 @@
+<header>
+	<nav>
+		<ul>
+			<li><a href="/">SpaceX Expolorers</a></li>
+			<li><a href="/about">About</a></li>
+		</ul>
+	</nav>
+</header>
---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About</h1>
+
+<p>This is an about page</p>
